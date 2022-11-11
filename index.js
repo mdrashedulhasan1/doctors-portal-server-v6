4cb1a307b2667818134624478aea4cbe83bdad5c
unknown <rashedul15-1718@diu.edu.bd>
2022-11-12 01:56:00 +0600

    Dashboard Server Code Done

--- index.js
+++ index.js
@@ -19,6 +19,34 @@ async function run() {
             const services = await cursor.toArray()
             res.send(services)
         });
+        app.get('/booking',async(req, res)=>{
+            const patient = req.query.patient;
+            const query = {patient: patient};
+            const bookings = await bookingCollection.find(query).toArray();
+            res.send(bookings)
+        })
+        app.get('/available',async(req,res)=>{
+            const date = req.query.date;
+            //Step1: Get all services
+            const services = await serviceCollection.find().toArray();
+
+            //Step2: Get the booking of the day
+            const query = {date:date};
+            const bookings = await bookingCollection.find(query).toArray();
+
+            //Step3:For each service
+            services.forEach(service => {
+                //Step4: Find booking for that service. output: [{}, {}, {}, {}]
+                const serviceBookings = bookings.filter(book => book.treatment === service.name);
+                //Step5: select slots for the service Bookings ['','',''];
+                const bookedSlots = serviceBookings.map(book => book.slot);
+                //Step6: select those slots thats are not in bookedSlots
+                const available = service.slots.filter(slot => !bookedSlots.includes(slot));
+                //Step7: set available to slots to make 
+                service.slots = available;
+            })
+        })
+
         app.post('/booking',async(req,res)=>{
             const booking = req.body;
             const query = {treatment:booking.treatment, date:booking.date, patient:booking.patient};
